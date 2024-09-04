39a7ed74aca2a0b63d7aa9b88a41296cee34138a
Ylin <1852847329@qq.com>
2024-09-04 21:49:54 +0800

    啊啊啊

---
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -0,0 +1,11 @@
+package com.sky.controller.admin;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/admin/setmeal")
+@Api(tags = "后台-套餐管理")
+public class SetmealController {
+}
